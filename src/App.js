6e83c578dad308da693a8c9ae2e58426b4f2775b
sonu4210 <souravjha497@gmail.com>
2021-08-07 04:14:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = [
+  { id:1, name:'john'},
+  {id:2, name:'peter'},
+  {id:3, name:'jack'},
+  {id:4,name:'dave'},
+];
 
-export default App;
+const ArrayExample = () =>{
+
+  const [people ,setPeople]= useState(data);
+
+  const removeItem = (id) =>{
+    let newpeople = people.filter((person)=>person.id !== id);
+    setPeople(newpeople);
+  };
+
+
+return (
+  <>
+  {people.map((person) =>{
+    const {id,name}=person;
+
+    return(
+      <div key={id} className='item'>
+        <h4>{name}</h4>
+        <button onClick={() => removeItem(id)}>remove item</button>
+      </div>
+    )
+  })}
+  <button className='btn' onClick={()=>setPeople([])}>clear items</button>
+  </>
+);
+};
+export default ArrayExample
